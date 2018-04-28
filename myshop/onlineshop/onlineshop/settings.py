e368b5c6862c8072cfc4bd40d54c0b996b10e2e3
unknown <jyotisangekarz@gmail.com>
2018-04-27 23:44:22 -0500

    Removed Account

--- myshop/onlineshop/onlineshop/settings.py
+++ myshop/onlineshop/onlineshop/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
     'shop',
     'cart',
     'orders',
